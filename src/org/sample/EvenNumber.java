80c4260a87fc49c743294b214603ddc4de7e5d22
charu93 <charmila0409@gmail.com>
2023-12-14 15:05:38 +0530

    Going to push this

---
+++ src/org/sample/EvenNumber.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import java.util.Iterator;
+
+public class EvenNumber {
+public static void main(String[] args) {
+	for (int i = 0; i <=20; i++) {
+		if (i%2==0) {
+			System.out.println(i);
+		}
+	}
+}
+}
